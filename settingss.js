d0876ceaec2cf3a976e1c06be10e7342949b44b4
Nimeshamadhushan <89959527+Nimeshamadhushan@users.noreply.github.com>
2025-08-02 00:44:04 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: "malvin~0iNEDCaC#jBNl7JaovYjYWl6OAvw_moZdRkjaxjfGkofsR_LK10U",  // add your Session Id make sure it starts with nima
   OWNER_NUMBER: "94726800969", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
