04d2ffd72b9202225fc4b9b671f88cc4240ca582
۝⃟̥̩̩̩̥̩̥͚̮♥නිමේෂ♥😝™ࣩ࿔ࣰ🎓❁ཻ͜͡彡😄꧁⃟⃞❀༂❃⃟𝙉𝙄⃞𝙈𝙀𝙎⃟𝙃𝘼❃❀⃟꧂۝⃟̥̩̩̩̥̩̥͚̮ <121599526+nimesha206@users.noreply.github.com>
2025-08-02 00:19:17 +0530

    Add files via upload

---
+++ settingss.js
@@ -0,0 +1,5 @@
+module.exports = {
+  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "94726800969", // PUT OWNER NUMBER HERE
+  PREFIX: "." // prefix (e.g., ., /, !, *)
+};
